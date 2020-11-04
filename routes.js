1c131b58da8e41f05138c5466e2d304eab8c4bab
vickicres <66817698+vickicres@users.noreply.github.com>
2020-11-04 12:24:05 -0800

    Updated routes.js

--- routes.js
+++ routes.js
@@ -157,53 +157,55 @@ router.get('/courses/:id', asyncHandler(async(req, res) => {
 }));
 
 // send a POST request to /courses 201 to create a course, sets the Location header to the URI for the course, and returns no content
-router.post('/courses', 
-[
-  check("title")
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "title"'),
-  check("description")
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "description"'),
-],
-
-authenticateUser, asyncHandler(async(req, res) => {
-  const course = await Course.create(req.body);
-  if (course) {
-    res.status(201).location('/courses' + course.id).end();
-  } else {
-    res.status(400).json({message: "course info is required."});
-  }
-}));
+router.post(
+  "/courses",
+  [
+    check("title")
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "title"'),
+    check("description")
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "description"'),
+  ],
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
+    const course = await Course.create(req.body);
+    res.location(`/courses/${course.id}`);
+    return res.status(201).end();
+  })
+);
 
 // send a PUT request to /courses/:id 204 to update (edit) a course and returns no content
-router.put('/courses/:id', authenticateUser,  [ 
-  check('title')
-      .exists()
-      .withMessage('Please provide a title'),
-  check('description')
-      .exists()
-      .withMessage('Please provide a description')
-], asyncHandler(async(req, res) => {
-  // get validation result from req object
-  const errors = validationResult(req);
-  // if there are validation errors
-  if (!errors.isEmpty()) {
-    //use array Map() method to get a list of error messages
-    const errorMessages = errors.array().map(error => error.msg);
-    //return the validation errors to the client
-    res.status(400).json({ errors: errorMessages });
-  } else {
-    const authenticatedUser = req.currentUser;
+router.put(
+  "/courses/:id",
+  [
+    check("title")
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "title"'),
+    check("description")
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('Please provide a value for "description"'),
+  ],
+  authenticateUser, asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
     const course = await Course.findByPk(req.params.id);
-    if (authenticatedUser.id = course.userId) {
+    if (course.userId === req.currentUser.id) {
       await course.update(req.body);
       res.status(204).end();
     } else {
       res.status(403).end();
     }
-  }
-}));
+  })
+);
 
 // send a DELETE request to /courses/:id 204 to delete a course and return no content
 router.delete('/courses/:id', authenticateUser, asyncHandler(async(req, res) => {
